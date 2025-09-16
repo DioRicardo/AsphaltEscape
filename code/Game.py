e6070d66cc2220c8eb64a18d3424f0ec62b24ea6
Dio Ricardo <dio.vieira@live.com>
2025-09-15 22:48:47 -0300

    Minor Text Improvements

--- code/Game.py
+++ code/Game.py
@@ -25,6 +25,7 @@ class Game:
                 player_score = level.run()
                 if player_score:
                     score.save(player_score)
+                    menu.run()
             elif menu_return == MENU_OPTION[1]:
                 score.show()
             elif menu_return == MENU_OPTION[2]:
